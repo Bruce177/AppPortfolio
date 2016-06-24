9f7c36c54e29d440cb3f8565bd01d151e451a13d
Bruce177 <yuqiqi17786@163.com>
2016-06-24 10:13:22 +0800

    项目0

--- app/src/main/java/com/google/yuqiqi/appportfolio/MainActivity.java
+++ app/src/main/java/com/google/yuqiqi/appportfolio/MainActivity.java
@@ -2,6 +2,8 @@ package com.google.yuqiqi.appportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showToastPopularMovie(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),R.string.PopularMovieMessage,Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+    public void showToastStoctHawk(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),R.string.StoctHawkMessage,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void showToastBuildItBigger(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),R.string.BuildItBiggerMessage,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void showToastMakeYourAppMaterial(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),R.string.MakeYourAppMaterialMessage,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void showToastGoUbiquitous(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),R.string.GoUbiquitousMessage,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void showToastCatstone(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),R.string.CatstoneMessage,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
 }
